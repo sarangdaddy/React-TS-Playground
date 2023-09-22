6394c6542e820c67c47e9bb19f0f47b5ee8a0897
sarang_daddy <sarang_daddy@naver.com>
2023-09-22 16:17:40 +0900

    Chore : CSS reset 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import { ThemeProvider } from 'styled-components';
-import theme from '@/theme';
 import { RouterProvider } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from './styles/GlobalStyle';
+import theme from '@/styles/theme';
 import router from './router';
 
 function App() {
   return (
     <ThemeProvider theme={theme.light}>
+      <GlobalStyle />
       <RouterProvider router={router} />
     </ThemeProvider>
   );
