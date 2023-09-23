6d842864e6987d034e2e1870d0a5a30575fdd12b
sarang_daddy <sarang_daddy@naver.com>
2023-09-23 20:59:26 +0900

    Design : CSS 초기화 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { ThemeProvider } from 'styled-components';
-import theme from '@/theme';
+import theme from '@/styles/theme';
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
+import GlobalStyle from './styles/GlobalStyle';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <RouterProvider router={router} />
     </ThemeProvider>
   );
