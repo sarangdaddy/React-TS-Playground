366d2069fdbfbcf36befe56553f2404437641082
sarang_daddy <sarang_daddy@naver.com>
2023-10-01 04:10:35 +0900

    Feat : modal-animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,91 +1,72 @@
 import styled from 'styled-components';
 import { AnimatePresence, motion } from 'framer-motion';
-import { useCallback, useState } from 'react';
-import _ from 'lodash';
+import { useState } from 'react';
 
 const Wrapper = styled.div`
   height: 100vh;
   width: 100vw;
   display: flex;
-  justify-content: center;
+  justify-content: space-around;
   align-items: center;
-  flex-direction: column;
 `;
 const Box = styled(motion.div)`
-  width: 100px;
-  height: 100px;
-  background-color: rgba(255, 255, 255);
-  display: flex;
-  justify-content: center;
-  align-items: center;
+  background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
-  position: absolute;
-  top: 0;
+  height: 200px;
   box-shadow:
     0 2px 3px rgba(0, 0, 0, 0.1),
     0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const box = {
-  entry: (back: boolean) => ({
-    x: back ? -500 : 500,
-    opacity: 0,
-    scale: 0,
-  }),
-  center: {
-    x: 0,
-    opacity: 1,
-    scale: 1,
-    transition: {
-      duration: 0.3,
-    },
-  },
-  exit: (back: boolean) => ({
-    x: back ? 500 : -500,
-    opacity: 0,
-    scale: 0,
-    transition: {
-      duration: 0.3,
-    },
-  }),
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  width: 50vw;
+  gap: 10px;
+  div:first-child,
+  div:last-child {
+    grid-column: span 2;
+  }
+`;
+
+const Overlay = styled(motion.div)`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const overlay = {
+  hidden: { backgroundColor: 'rgba(0, 0, 0, 0)' },
+  visible: { backgroundColor: 'rgba(0, 0, 0, 0.5)' },
+  exit: { backgroundColor: 'rgba(0, 0, 0, 0)' },
 };
 
 function App() {
-  const [visible, setVisible] = useState(1);
-  const [back, setBack] = useState(false);
-
-  const nextPlease = useCallback(
-    _.debounce(() => {
-      setBack(false);
-      setVisible((prev) => (prev === 10 ? 10 : prev + 1));
-    }, 200),
-    [],
-  );
-
-  const prevPlease = useCallback(
-    _.debounce(() => {
-      setBack(true);
-      setVisible((prev) => (prev === 1 ? 1 : prev - 1));
-    }, 200),
-    [],
-  );
+  const [id, setId] = useState<null | string>(null);
 
   return (
     <Wrapper>
-      <AnimatePresence custom={back}>
-        <Box
-          custom={back}
-          variants={box}
-          initial="entry"
-          animate="center"
-          exit="exit"
-          key={visible}
-        >
-          {visible}
-        </Box>
+      <Grid>
+        {['1', '2', '3', '4'].map((n) => (
+          <Box onClick={() => setId(n)} key={n} layoutId={n} />
+        ))}
+      </Grid>
+      <AnimatePresence>
+        {id ? (
+          <Overlay
+            variants={overlay}
+            onClick={() => setId(null)}
+            initial="hidden"
+            animate="visible"
+            exit="exit"
+          >
+            <Box layoutId={id} style={{ width: 200, height: 200 }} />
+          </Overlay>
+        ) : null}
       </AnimatePresence>
-      <button onClick={nextPlease}>next</button>
-      <button onClick={prevPlease}>prev</button>
     </Wrapper>
   );
 }
